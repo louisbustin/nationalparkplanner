4fb162333986b609d37d120ecf41eb7a5b4edcf9
Louis Bustin <louis@eforge.us>
2025-08-07 08:48:18 -0500

    Lint format fix

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # National Park Planner
+
 This site is intended as a way for users to plan trips to national parks, including to which airports to fly to maximize the number of parks visited each trip
 
 ## Development
+
 This is based on Svelte 5, using SvelteKit. Deploy is using SST to AWS.
--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [
-		tailwindcss(),
-		sveltekit(),
-		devtoolsJson()
-	],
+	plugins: [tailwindcss(), sveltekit(), devtoolsJson()],
 	test: {
 		expect: { requireAssertions: true },
 		projects: [
